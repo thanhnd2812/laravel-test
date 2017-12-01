ff4674716c8d40d734514d09e9a22ebc8cf9ab46
njt28 <thanh.nguyen@zorpiahq.com>
2017-12-01 14:52:06 +0700

    convert csv to sqlite

---
+++ app/Http/Controllers/SaleController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sale;
+use Illuminate\Http\Request;
+use Mockery\Exception;
+
+class SaleController extends Controller
+{
+    public function __construct() {
+
+    }
+
+    public function index() {
+        $this->seeding();
+        $sales =  Sale::all();
+
+        return view('sales.index', compact('sales'));
+    }
+
+
+    private function seeding() {
+
+        if (count(Sale::all())) {
+            return;
+        }
+
+        $path = public_path()  . "/data.csv" ;
+
+        if ( ( $handle = fopen($path, 'r')) !== FALSE) {
+            $index = 0;
+
+            while (( $data = fgetcsv($handle, 10000)) !== FALSE) {
+                if ($index++ <= 0) {
+                    continue;
+                }
+                if (Sale::where('id', '=', $data[0])->exists() ) {
+                    continue;
+                }
+                $record = new Sale;
+                $record->id = $data[0];
+                $record->neighborhood = $data[12];
+                $record->price = $data[16];
+                $record->bedroom = $data[17];
+                $record->badroom = $data[18];
+                $record->type = $data[11];
+                $record->address = $data[2];
+                try {
+                    $record->save();
+                }catch (Exception $e) {
+
+                }
+            }
+            fclose($handle);
+        }
+    }
+}
---
+++ database/migrations/2017_12_01_071429_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('address')->unique();
+            $table->decimal('price')->default(0);
+            $table->float('bedroom')->default(0);
+            $table->float('badroom')->dafault(0);
+            $table->string('type');
+            $table->string('neighborhood');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
