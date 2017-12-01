3ee15da352ba7de2c3419f102b9241ffdc779571
njt28 <thanh.nguyen@zorpiahq.com>
2017-12-01 18:40:33 +0700

    change the routes

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SaleController@index');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/sales', 'SaleController@index');
